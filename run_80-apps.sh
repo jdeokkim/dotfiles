05ffa9378191bfe36a1e796572dfd7a9756d287c
Jaedeok Kim <jdeokkim@protonmail.com>
2026-05-03 21:16:13 +0900

    Update dot_config/zed/private_settings.json

--- run_80-apps.sh
+++ run_80-apps.sh
@@ -3,7 +3,7 @@
 # ============================================================================>
 
 sudo xbps-install Thunar alacritty feh firefox-esr flatpak grim nwg-look \
-    slurp swappy thunar-archive-plugin void-repo-nonfree xarchiver \
+    slurp strace swappy thunar-archive-plugin void-repo-nonfree xarchiver \
     xdg-desktop-portal-wlr
 
 # ============================================================================>
