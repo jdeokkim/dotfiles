700857130da663eea12684eb94b0722893ac6d48
Jaedeok Kim <jdeokkim@protonmail.com>
2026-05-18 00:53:43 +0900

    Update run_80-apps.sh

--- run_80-apps.sh
+++ run_80-apps.sh
@@ -5,7 +5,7 @@
 sudo xbps-install Thunar alacritty brightnessctl cscope ctags feh \
     firefox-esr flatpak grim nwg-look okular slurp strace swappy \
     thunar-archive-plugin void-repo-nonfree vscode xarchiver \
-    xdg-desktop-portal-wlr
+    xdg-desktop-portal-gtk xdg-desktop-portal-wlr
 
 # ============================================================================>
 
