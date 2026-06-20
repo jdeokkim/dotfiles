17fa6f5d19965885ff33d9c3ae72ed51e559e6c1
Jaedeok Kim <jdeokkim@protonmail.com>
2026-06-20 22:14:58 +0900

    Update run_01-grub.sh

--- run_01-grub.sh
+++ run_01-grub.sh
@@ -2,15 +2,25 @@
 
 # ============================================================================>
 
-sudo xbps-install -S base-devel git intel-ucode
+sudo xbps-install -S base-devel git intel-ucode nftables runit-nftables
 
 # ============================================================================>
 
-if [ ! -d "/tmp/grub-theme" ]; then
+if [ ! -d "/tmp/grub-theme/" ]; then
     git clone https://github.com/jdeokkim/minimal-grub-theme \
         /tmp/grub-theme
 fi
 
 make install -C /tmp/grub-theme
+
+# ============================================================================>
+
+if [ -n "$CHEZMOI_SOURCE_DIR" ]; then
+    sudo cp -i "$CHEZMOI_SOURCE_DIR/.root-config/etc/nftables.conf" /etc/
+fi
+
+if [ ! -d "/var/service/nftables/" ]; then
+    sudo ln -s /etc/sv/nftables /var/service/
+fi
 
 # ============================================================================>
