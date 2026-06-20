34ac743b81a907ada6d39f13d151b08220cacedf
Jaedeok Kim <jdeokkim@protonmail.com>
2026-06-20 22:18:01 +0900

    Add run_02-nftables.sh

--- run_02-nftables.sh
+++ run_02-nftables.sh
@@ -2,16 +2,16 @@
 
 # ============================================================================>
 
-sudo xbps-install -S base-devel git intel-ucode
+sudo xbps-install -S nftables runit-nftables
 
 # ============================================================================>
 
-if [ ! -d "/tmp/grub-theme/" ]; then
-    git clone https://github.com/jdeokkim/minimal-grub-theme \
-        /tmp/grub-theme
+if [ -n "$CHEZMOI_SOURCE_DIR" ]; then
+    sudo cp -i "$CHEZMOI_SOURCE_DIR/.root-config/etc/nftables.conf" /etc/
 fi
 
-make install -C /tmp/grub-theme
+if [ ! -d "/var/service/nftables/" ]; then
+    sudo ln -s /etc/sv/nftables /var/service/
+fi
 
 # ============================================================================>
-
